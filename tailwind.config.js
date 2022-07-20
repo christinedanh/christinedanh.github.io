dda2da5871eceda7460c4b82a6bc9b88abca9c69
Peter Kim <p@peter-kim.com>
2022-07-19 17:28:53 -0700

    IP

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,28 +13,6 @@ module.exports = {
         "light-primary": '#ECEDEF',
         "dark-primary": '#2E2E31',
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            pre: {
-              padding: "0",
-              color: "#1F2933",
-              backgroundColor: "#F3F3F3"
-            },
-            code: {
-              padding: "0.2em 0.4em",
-              backgroundColor: "#F3F3F3",
-              color: "#DD1144",
-              fontWeight: "400",
-              "border-radius": "0.25rem"
-            },
-            "code::before": false,
-            "code::after": false,
-            "blockquote p:first-of-type::before": false,
-            "blockquote p:last-of-type::after": false,
-          },
-        },
-      },
       fontFamily: {
         sans: ['Red Hat Text', ...defaultTheme.fontFamily.sans],
         serif: ['Lora', ...defaultTheme.fontFamily.serif],
